8dc528ed886b2128b9c50d7400a4a69a80f44741
Nilesh Lagdhir <nlagdhir@gmail.com>
2022-11-08 20:03:55 +0530

    get term from input and redirect to the related page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,22 @@ import Image from "next/image";
 import Header from "../components/header";
 import { MagnifyingGlassIcon, MicrophoneIcon } from "@heroicons/react/24/solid";
 import Footer from "../components/footer";
+import { useRouter } from "next/router";
+import { useRef } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (event) => {
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+    if(!term.trim()) return;
+
+    router.push(`/search?term=${term.trim()}`);
+
+  }
+
   return (
     <div>
       <Head>
@@ -29,11 +43,11 @@ export default function Home() {
 
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
           <MagnifyingGlassIcon className="h-5 text-gray-500 mr-3" />
-          <input type="text" className="flex-grow focus:outline-none" />
+          <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
           <MicrophoneIcon className="h-5" />
         </div>
         <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={search} className="btn">Google Search</button>
           <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
